fa8b6aa206833ad38a937f4c894a971482494f77
mcao610 <c_meng@outlook.com>
2022-03-07 23:09:54 -0500

    first commit

---
+++ model.py
@@ -0,0 +1,232 @@
+import math
+import torch
+import torch.nn as nn
+
+from fairseq.data.data_utils import collate_tokens
+from transformers import BartTokenizer
+
+
+class ConditionalSequenceGenerator:
+    """Conditional sequence generator for calculating prior and posterior probability."""
+    def __init__(self, bart):
+        self.bart = bart
+        self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large", local_files_only=False)
+        
+        self.encode_func = bart.encode
+        self.decode_func = bart.decode
+        self.max_positions = bart.max_positions
+        self.encode_line = bart.task.source_dictionary.encode_line
+        
+        self._initialize()
+    
+    def _initialize(self):
+        """Set BART model to evaluation mode."""
+        self.bart.cuda()
+        self.bart.eval()
+        self.bart.half()
+        
+    def tokenize_target(self, input_str, left_pad=False, append_eos=False):
+        """BPE-encode a sentence (or multiple sentences).
+
+        Args:
+            input_str (str or List[str]): input sentence to be tokenized.
+            left_pad (bool): self-explained.
+
+        Return:
+            prev_output_tokens (torch.Tensor): [batch_size, length]
+            target (torch.Tensor): [batch_size, length]
+            tgt_lengths (torch.Tensor): [batch_size]
+            
+        """
+        if type(input_str) == type(''):
+            input_str = [input_str]
+
+        prev_ids, tgt_ids = [], []
+
+        for ins in input_str:
+            tokens = self.bart.bpe.encode(ins)  # <mask>: 1279 27932 29
+            calibration = 1
+            if len(tokens.split(" ")) > min(self.max_positions) - calibration:
+                tokens = " ".join(tokens.split(" ")[: min(self.max_positions) - calibration])
+            
+            if append_eos:
+                tokens = "<s> " + tokens
+            prev_tokens = "</s> " + tokens
+            tgt_tokens = tokens + " </s>"
+            
+            tgt_ids.append(self.encode_line(tgt_tokens, append_eos=False).long())
+            prev_ids.append(self.encode_line(prev_tokens, append_eos=False).long())
+
+        prev_output_tokens = collate_tokens(prev_ids, pad_idx=1, left_pad=left_pad).cuda()
+        target = collate_tokens(tgt_ids, pad_idx=1, left_pad=left_pad).cuda()
+        tgt_lengths = torch.sum(target != 1, dim=1).cuda()
+
+        return prev_output_tokens, target, tgt_lengths
+        
+    def tokenize(self, input_str, append_bos=False, append_eos=True, left_pad=True):
+        """BPE-encode a sentence (or multiple sentences).
+
+        Args:
+            input_str (str or List[str]): input sentence to be tokenized.
+            append_bos (bool): self-explained.
+            append_eos (bool): self-explained.
+
+        Return:
+            input_ids (torch.Tensor): [batch_size, length]
+            src_lengths (torch.Tensor): [batch_size]
+        """
+        if type(input_str) == type(''):
+            input_str = [input_str]
+
+        input_ids = []
+        for ins in input_str:
+            tokens = self.bart.bpe.encode(ins)  # <mask>: 1279 27932 29
+            calibration = sum([append_bos, append_eos])
+            if len(tokens.split(" ")) > min(self.max_positions) - calibration:
+                tokens = " ".join(tokens.split(" ")[: min(self.max_positions) - calibration])
+
+            tokens = "<s> " + tokens if append_bos else tokens
+            tokens = tokens + " </s>" if append_eos else tokens
+            ids = self.encode_line(tokens, append_eos=False).long()
+            input_ids.append(ids)
+
+        input_ids = collate_tokens(input_ids, pad_idx=1, left_pad=left_pad).cuda()
+        input_lengths = torch.sum(input_ids != 1, dim=1).cuda()
+
+        return input_ids, input_lengths
+    
+    def tokenize_with_mask(self, input_str):
+        """Tokenize sentence with a special <mask> token in it.
+
+        Args:
+            input_str (str or List[str]): input sentence to be tokenized.
+
+        Return:
+            input_ids (torch.Tensor): [batch_size, length]
+            src_lengths (torch.Tensor): [batch_size]
+        """
+        input_ids = self.tokenizer(input_str, return_tensors='pt', padding=True)['input_ids'].cuda()
+        input_lengths = torch.sum(input_ids != 1, dim=1).cuda()
+        return input_ids, input_lengths
+    
+    def encode_decode(self, src_input, tgt_input, mask_filling=False):
+        """
+        Args:
+            src_input: (List[str])
+            tgt_input: (List[str])
+            
+        """
+        if mask_filling:
+            src_tokens, src_lengths = self.tokenize_with_mask(src_input)
+            prev_output_tokens, target, tgt_lengths = self.tokenize_target(tgt_input, left_pad=False, append_eos=True)
+        else:
+            src_tokens, src_lengths = self.tokenize(src_input, append_bos=False)
+            prev_output_tokens, target, tgt_lengths = self.tokenize_target(tgt_input, left_pad=False)
+        
+        with torch.no_grad():
+            encoder_out = self.bart.model.encoder(src_tokens, src_lengths=src_lengths)
+            decoder_out = self.bart.model.decoder(prev_output_tokens, encoder_out=encoder_out, features_only=False)
+
+            probs = nn.functional.softmax(decoder_out[0], dim=-1)
+            tgt_token_probs = torch.gather(probs, 2, target.unsqueeze(-1)).squeeze(2)
+
+            # mask <pad> with probability 1.0
+            max_tgt_length = tgt_lengths.max().item()
+            tgt_lengths = tgt_lengths - 1
+            tgt_mask = torch.arange(max_tgt_length)[None, :].cuda() < tgt_lengths[:, None]
+            tgt_token_probs.masked_fill_(tgt_mask == False, 1.0)
+
+        return tgt_token_probs, target
+    
+    def generate(self, src_input, tgt_input=None):
+        """Conditional generation.
+        
+        Args:
+            src_input (str or List[str]): input source sentence to be tokenized.
+            tgt_input (str or List[str]): input target sentence to be tokenized.
+        """
+        input_ids, lengths = self.tokenize(src_input, append_bos=False) 
+        
+        target_ids = None
+        if tgt_input is not None:
+            assert len(src_input) == len(tgt_input), "source & target length should match."
+            target_ids, _ = self.tokenize(tgt_input, append_bos=False, left_pad=False)
+        
+        with torch.no_grad():
+            encoder_output = self.encode_sequence(input_ids, lengths)
+            decoder_output = self.decode_sequence(encoder_output, 
+                                                  target_ids=target_ids,
+                                                  prefix_tokens=[2])
+        return decoder_output
+    
+    def mask_filling(self, src_input, tgt_input=None):
+        """
+        Filling the mask in sentence(s).
+        """
+        input_ids, lengths = self.tokenize_with_mask(src_input)
+        
+        target_ids = None
+        if tgt_input is not None:
+            assert len(src_input) == len(tgt_input), "source & target length should match."
+            target_ids, _ = self.tokenize(tgt_input, left_pad=False)
+
+        with torch.no_grad():
+            encoder_output = self.encode_sequence(input_ids, lengths)
+            decoder_output = self.decode_sequence(encoder_output, 
+                                                  target_ids=target_ids,
+                                                  prefix_tokens=[2, 0])
+        return decoder_output
+    
+    def encode_sequence(self, input_ids, lengths):
+        return self.bart.model.encoder(input_ids, src_lengths=lengths)
+        
+    def decode_sequence(
+        self,
+        encoder_out,
+        target_ids=None,
+        min_decode_step=3,
+        max_decode_step=100,
+        pad_id=1,
+        eos_id=2,
+        prefix_tokens=[2, 0],
+    ):
+        batch_size = encoder_out['encoder_padding_mask'][0].shape[0]
+        init_input = torch.tensor([prefix_tokens] * batch_size, dtype=torch.long).cuda()
+        token_probs, tokens = None, [[] for i in range(batch_size)]
+        end_mask = torch.tensor([False] * batch_size).cuda()
+
+        softmax = nn.Softmax(dim=1)
+        for step in range(max_decode_step):
+            decoder_outputs = self.bart.model.decoder(init_input, encoder_out, features_only=False)
+            logits = decoder_outputs[0][:, -1, :]  # logits: [batch_size, vocab]
+            attn = decoder_outputs[1]['attn'][0]  # [batch_size, prev_token_len, src_token_len]
+
+            if step + 1 < min_decode_step:
+                logits[:, eos_id] = -math.inf  # mask <EOS> token when within minimal step
+            logits[:, pad_id], logits[:, 0] = -math.inf, -math.inf  # never select <PAD> & <BOS> token
+            probs = softmax(logits)  # probs: [batch_size, vocab]
+
+            # select tokens
+            if target_ids is not None:
+                selected_token = target_ids[:, step]
+            else:
+                value, indices = torch.topk(probs, 5, dim=1)
+                selected_token = indices[:, 0]
+
+            selected_token = selected_token.masked_fill(end_mask, pad_id)
+            init_input = torch.cat([init_input, selected_token.unsqueeze(1)], dim=-1)
+            
+            probs = torch.gather(probs, 1, selected_token.unsqueeze(1)).detach()
+            probs = probs.masked_fill(end_mask.unsqueeze(1), 1.0)
+            
+            # str & probability
+            token_probs = probs if token_probs is None else torch.cat([token_probs, probs], dim=-1)
+            for t, s in zip(tokens, selected_token):
+                t.append(self.decode_func(s.unsqueeze(0)) if s.item() != pad_id else '<pad>')
+            
+            # stop generation when all finished
+            end_mask = torch.logical_or(end_mask, selected_token == eos_id) 
+            if end_mask.sum().item() == batch_size:
+                break
+
+        return init_input, tokens, token_probs
---
+++ utils.py
@@ -0,0 +1,203 @@
+import os
+import json
+import torch
+
+def read_lines(file_path):
+    files = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            files.append(line.strip())
+    return files
+
+
+def read_jsonl(file_path):
+    data = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            data.append(json.loads(line.strip()))
+    return data
+
+
+def get_probability(position, tokens, probs, entity):
+    """Calculate the probability of a span.
+
+    Args:
+        position: (start, end)
+        tokens: ['The', ' Archbishop', ' of', ...]
+        probs: [0.50, 0.49, 0.88, ...]
+        entity: Rodgers
+    """
+    assert len(tokens) == len(probs), "Tokens and token probabilities does not match."
+    
+    end_pointer, end_pos = 0, []
+    for t in tokens:
+        end_pointer += len(t)
+        end_pos.append(end_pointer)
+    
+    assert position[1] in end_pos, "- {}\n- {}\n- {}\n- {}\n- {}\n".format(position, tokens, probs, entity, end_pos)
+    last_index = end_pos.index(position[1])
+    indexes = [last_index]
+    total_length = len(tokens[last_index])
+    
+    while total_length < (position[1] - position[0]):
+        last_index -= 1
+        assert last_index >= 0
+        indexes.append(last_index)
+        total_length += len(tokens[last_index])
+    
+    indexes.reverse()
+    
+    generated = ''.join([tokens[i] for i in indexes])
+    assert entity in generated, 'entity: {}; span: {}'.format(entity, generated)
+    
+    prob = 1.0
+    for i in indexes:
+        prob *= probs[i]
+    return prob
+
+
+def get_cmlm_probability(generator, src_input, tgt_input, position, entity):
+    outputs = generator.generate(src_input, tgt_input=tgt_input)
+    init_input, tokens, token_probs = outputs
+    
+    probs = []
+    for p, tok, tokp, e in zip(position, tokens, token_probs, entity):
+        probs.append(get_probability(p, tok, tokp, e).item())
+    
+    return probs
+
+
+def get_probability_parallel(generator, src_input, tgt_input, position, entity, mask_filling=False):
+    """Get entities probability in parallel decoding.
+
+    Args:
+        generator: model
+        args*: outputs from prepare_cmlm_inputs()
+
+    """
+    token_probs, target = generator.encode_decode(src_input, tgt_input=tgt_input, mask_filling=mask_filling)
+
+    probs = []
+    for p, tok, tokp, e in zip(position, target, token_probs, entity):
+        if mask_filling:
+            assert tok[0].item() == 0
+            tok, tokp = tok[1:], tokp[1:]
+        tok = [generator.decode_func(i.unsqueeze(0)) for i in tok]
+        probs.append(get_probability(p, tok, tokp, e).item())
+
+    return probs
+
+
+def get_prior_probability(generator, src_input, tgt_input, position, entity):
+    """Tokenize input with a special <mask> token."""
+    assert len(src_input) == len(tgt_input), "source & target length should match."
+    decoder_output = generator.mask_filling(src_input, tgt_input)
+    init_input, tokens, token_probs = decoder_output
+    
+    probs = []
+    for p, tok, tokp, e in zip(position, tokens, token_probs, entity):
+        probs.append(get_probability(p, tok, tokp, e).item())
+    return probs
+
+
+def prepare_clm_inputs(source, target, ent_parts=None):
+    """For Conditional Language Model. For XSum BART only."""
+    if ent_parts is None:
+        ent_parts = nlp(target).to_json()['ents']
+    
+    entities, positions = [], []
+    inputs, targets = [], []
+
+    for e in ent_parts:
+        inputs.append(source)
+        targets.append(target)
+        positions.append((e['start'], e['end']))
+        entities.append(target[e['start']: e['end']])
+
+    return inputs, targets, positions, entities
+
+
+def prepare_mlm_inputs(source, target, ent_parts=None):
+    """For Masked Language Model. For BART only."""
+    if ent_parts is None:
+        ent_parts = nlp(target).to_json()['ents']
+    
+    inputs, targets = [], []
+    positions, entities = [], []
+
+    for e in ent_parts:
+        inputs.append(target[0: e['start']] + '<mask>' + target[e['end']:])
+        targets.append(target)
+        entities.append(target[e['start']: e['end']])
+        positions.append((e['start'], e['end']))
+    
+    return inputs, targets, positions, entities
+
+
+def prepare_cmlm_inputs(source, target, ent_parts=None):
+    """For Conditional Masked Language Model."""
+    if ent_parts is None:
+        ent_parts = nlp(target).to_json()['ents']
+    
+    inputs, targets = [], []
+    positions, entities = [], []
+
+    for e in ent_parts:
+        masked_hypothesis = target[0: e['start']] + '###' + target[e['end']:]
+        masked_hypothesis = '<s> ' + masked_hypothesis + ' <\s> ' + source
+        inputs.append(masked_hypothesis)
+        targets.append('<s> ' + target)
+        
+        entities.append(target[e['start']: e['end']])
+        positions.append((e['start'] + 4, e['end'] + 4))
+
+    return inputs, targets, positions, entities
+
+
+def prepare_cmlm_ent_inputs(source, target, ent_parts=None):
+    """For Entity Conditional Masked Language Model."""
+    if ent_parts is None:
+        ent_parts = nlp(target).to_json()['ents']
+    
+    inputs, targets, entities = [], [], []
+
+    for e in ent_parts:
+        masked_hypothesis = target[0: e['start']] + '###' + target[e['end']:]
+        masked_hypothesis = '<s> ' + masked_hypothesis + ' <\s> ' + source
+        inputs.append(masked_hypothesis)
+        targets.append('<s> ' + target[e['start']: e['end']])
+        
+        entities.append(target[e['start']: e['end']])
+
+    return inputs, targets, entities
+
+
+def process_document(raw_doc):
+    TRIVIAL_SENTS = [
+        'Share this with',
+        'Copy this link',
+        'These are external links and will open in a new window',
+    ]
+    
+    raw_doc = raw_doc.strip()
+    raw_doc_sents = raw_doc.split('\n')
+    
+    start_signal = False
+    filtered_sentences = []
+    for s in raw_doc_sents: 
+        if start_signal:
+            filtered_sentences.append(s)
+        elif len(s.split()) > 1 and s not in TRIVIAL_SENTS:
+            start_signal = True
+            filtered_sentences.append(s)
+            
+    return ' '.join(filtered_sentences)
+
+
+def read_document(bbcid, folder):
+    file_path = folder + '{}.document'.format(bbcid)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return process_document(f.read())
+    else:
+        return None
