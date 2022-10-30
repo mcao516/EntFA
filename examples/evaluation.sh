ab16e6e9166aed6b3c7f23eab31daf514efca7ae
mcao610 <c_meng@outlook.com>
2022-10-30 14:05:11 -0400

    update evaluation script

--- examples/evaluation.sh
+++ examples/evaluation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+SOURCE_PATH=test.source
+TARGET_PATH=test.hypothesis
 
 python evaluation.py \
-    --source_path $SCRATCH/summarization/XSum/fairseq_files/test.source.100 \
-    --target_path $SCRATCH/summarization/XSum/fairseq_files/test.target.100 \
+    --source_path $SOURCE_PATH \
+    --target_path $TARGET_PATH \
     --cmlm_model_path $SCRATCH/BART_models/xsum_cedar_cmlm \
     --data_name_or_path $SCRATCH/summarization/XSum/fairseq_files/xsum-bin \
     --mlm_path $SCRATCH/BART_models/bart.large \
